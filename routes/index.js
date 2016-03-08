9b6db6a9a751c6f80f479480fbb0a7e2e4df61cd
dmand91 <dmand91@gmail.com>
2016-03-08 16:29:27 +0000

    final

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 
 
+
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
@@ -20,6 +21,30 @@ router.get('/', function(req, res, next) {
           //console.log(myRe);
 
              fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
+            if(err) throw err;
+            var cities = data.toString().split("\n");
+        	 var jsonresult = [];
+        for(var i = 0; i < cities.length; i++) {
+          var result = cities[i].search(myRe); 
+          if(result != -1) {
+           console.log(cities[i]);
+           jsonresult.push({city:cities[i]});
+          } 
+        }   
+        //console.log(jsonresult);
+        res.status(200).json(jsonresult);  
+	
+	});  
+
+});
+
+  router.get('/getpokemon',function(req,res,next) {
+		
+	  //console.log(req.query);		
+          var myRe = new RegExp("^" + req.query.q);
+          //console.log(myRe);
+
+             fs.readFile(__dirname + '/pokemon.txt',function(err,data) {
             if(err) throw err;
             var cities = data.toString().split("\n");
         	 var jsonresult = [];
